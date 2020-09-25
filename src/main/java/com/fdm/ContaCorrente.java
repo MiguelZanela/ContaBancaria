f8da7ce030b0dc5cd2ef43d9eaebe1ffb8fd58ae
Miguel Zanela <miguelzanela89@gmail.com>
2020-09-25 15:42:34 -0300

    classes implementadas, faltando apenas retirada, alguns testes ja aplicados

--- src/main/java/com/fdm/ContaCorrente.java
+++ src/main/java/com/fdm/ContaCorrente.java
@@ -1,26 +1,76 @@
 package com.fdm;
 
-enum Categoria {Silver, Gold, Platinum}
-
 public class ContaCorrente{
+  private double saldo;
+  private String numeroConta;
+  private String nomeCorrentista;
+  private String Silver = "Silver";
+  private String Gold = "Gold";
+  private String Platinum = "Platinum";
+  private String categoria;
+
+
+  public ContaCorrente(String numeroConta, String nomeCorrentista, double saldoInicial){
+    this.numeroConta = numeroConta;
+    this.nomeCorrentista = nomeCorrentista;
+    if(saldoInicial <= 0.0){
+      this.saldo = 0.0;
+      this.categoria = Silver;
+    }
+    else if(saldoInicial < 50000.0){
+      this.saldo = saldoInicial;
+      this.categoria = Silver;
+    }
+    else if(saldoInicial >= 50000.0 && saldoInicial < 200000.0){
+      this.saldo = saldoInicial;
+      this.categoria = Gold;
+    }
+    else{
+      this.saldo = saldoInicial;
+      this.categoria = Platinum;
+    }
+  }
+
   public String getNumeroConta(){
-    return "";
+    return this.numeroConta;
   }
 
   public String getNomeCorrentista(){
-    return "";
+    return this.nomeCorrentista;
   }
 
   public double getSaldo(){
-    return 0.0;
+    return this.saldo;
   }
 
-  public Categoria getCategoria(){
-    return null;
+  public String getCategoria(){
+    return this.categoria;
   }
 
   public boolean deposito(double valor){
-    return false;
+    if(valor <= 0){
+      return false;
+    }
+    else{
+      if(categoria.toUpperCase().equals(Silver.toUpperCase())){
+        this.saldo = this.saldo+valor;
+        if(this.saldo >= 50000.0){
+          this.categoria = Gold;
+        }
+        return true;
+      }
+      else if(categoria.toUpperCase().equals(Gold.toUpperCase())){
+        this.saldo = this.saldo+(valor*0.01+valor);
+        if(this.saldo >= 200000.0){
+          this.categoria = Platinum;
+        }
+        return true;
+      }
+      else{
+        this.saldo = this.saldo+(valor*0.025+valor);
+        return true;
+      }
+    }
   }
 
   public boolean retirada(double valor){
--- src/test/java/com/fdm/ContaCorrenteTest.java
+++ src/test/java/com/fdm/ContaCorrenteTest.java
@@ -1,3 +1,45 @@
 package com.fdm;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class ContaCorrenteTest{
+    
+    @ParameterizedTest
+    @CsvSource({
+        "0.0,5000.0,Silver,5000.0",
+        "20000.0,6000.0,Silver,26000.0",
+        "200000.0,1000.0,Platinum,201025.0"
+    })
+    public void testaDeposito(double saldoInicial, double deposito, String estadoEsperado, double saldoEsperado){
+        // Monta cenário
+        ContaCorrente ct = new ContaCorrente("00","joao de oliveira",saldoInicial);
+        // Executa o teste
+        ct.deposito(deposito);
+        double saldoObservado = ct.getSaldo();
+        String estadoObservado = ct.getCategoria();
+        // Verifica resultados esperados
+        assertEquals(saldoEsperado,saldoObservado,0.001);
+        assertEquals(estadoEsperado,estadoObservado);
+    } 
+    
+    /*
+    @ParameterizedTest
+    @CsvSource({
+        "0.0,5000.0,Silver,5000.0",
+        "20000.0,6000.0,Gold,26000.0"
+    })
+    public void testaRetirada(double saldoInicial, double retirada, String estadoEsperado, double saldoEsperado){
+        // Monta cenário
+        ContaCorrente ct = new ContaCorrente("00","joao de oliveira",saldoInicial);
+        // Executa o teste
+        ct.deposito(retirada);
+        double saldoObservado = ct.getSaldo();
+        String estadoObservado = ct.getCategoria();
+        // Verifica resultados esperados
+        assertEquals(saldoEsperado,saldoObservado,0.001);
+        assertEquals(estadoEsperado,estadoObservado);
+    } 
+    */
+  }
